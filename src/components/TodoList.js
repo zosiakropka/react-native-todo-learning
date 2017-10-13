23a2fd6e03fe666cdd3b6a79e7d318969faa6b19
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-10-13 10:09:20 +0200

    Make it possible to scroll the TODO list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View } from 'react-native'
+import { FlatList } from 'react-native'
 
 import TodoItem from './TodoItem'
 
 const TodoList = ({todos, onTodoItemClick}) => (
-  <View>
-    {
-      todos.map(todo => (
-        <TodoItem
-          key={todo.id}
-          completed={todo.completed}
-          text={todo.text}
-          imageSource={todo.imageSource}
-          onClick={() => onTodoItemClick(todo.id)} />
-      ))
-    }
-  </View>
+  <FlatList
+    data={todos}
+    renderItem={({item}) => (
+      <TodoItem
+        key={item.id}
+        completed={item.completed}
+        text={item.text}
+        imageSource={item.imageSource}
+        onClick={() => onTodoItemClick(item.id)} />
+    )} />
 )
 
 TodoList.propTypes = {
