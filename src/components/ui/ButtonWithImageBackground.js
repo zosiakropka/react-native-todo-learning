ac93c8f4b59cff065ce1f0fb7da574c17934ff42
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-10-14 02:26:30 +0200

    Animate ButtonWithImageBackground by extending its height on show

--- src/components/ui/ButtonWithImageBackground.js
+++ src/components/ui/ButtonWithImageBackground.js
@@ -1,7 +1,11 @@
-import React, { PropTypes } from 'react'
-import { TouchableOpacity, Image, View, Text } from 'react-native'
+import React, { PropTypes, Component } from 'react'
+import { TouchableOpacity, Image, View, Text, Animated } from 'react-native'
 
 import { FLEX_1_STYLE, ABSOLUTE_VIEW_STYLE } from '../../styles/generic'
+
+const ANIMATION_INITIAL_VALUE = 0
+const ANIMATION_FINAL_VALUE = 1
+const ANIMATION_DURATION_MS = 150
 
 const styles = {
   absoluteView: {
@@ -15,32 +19,60 @@ const styles = {
     height: 100
   },
   text: {
-    color: 'white'
+    color: 'white',
+    textShadowColor: 'black',
+    textShadowRadius: 10,
+    textShadowOffset: {
+      width: 0,
+      height: 0
+    }
   },
   withPadding: {
     padding: 20
   }
 }
 
-const ButtonWithImageBackground = ({backgroundSource, onClick, children}) => (
-  <TouchableOpacity onPress={onClick} style={styles.button}>
-    <Image
-      resizeMode={'cover'}
-      source={backgroundSource}
-      style={FLEX_1_STYLE}
-      alignSelf={'stretch'}
-      width={undefined}
-      height={undefined} />
-    <View style={{...styles.absoluteView, ...styles.withPadding}}>
-      <Text
-        textShadowRadius={4}
-        numberOfLines={2}
-        style={styles.text}>
-        {children}
-      </Text>
-    </View>
-  </TouchableOpacity>
-)
+export default class ButtonWithImageBackground extends Component {
+  animation = new Animated.Value(ANIMATION_INITIAL_VALUE)
+
+  componentDidMount () {
+    Animated.timing(this.animation, {
+      toValue: ANIMATION_FINAL_VALUE,
+      duration: ANIMATION_DURATION_MS
+    })
+  }
+
+  render () {
+    const {backgroundSource, onClick, children} = this.props
+
+    return (
+      <TouchableOpacity onPress={onClick} style={styles.button}>
+        <Image
+          resizeMode={'cover'}
+          source={backgroundSource}
+          style={FLEX_1_STYLE}
+          alignSelf={'stretch'}
+          width={undefined}
+          height={undefined} />
+        <View style={{...styles.absoluteView, ...styles.withPadding}}>
+          <Text
+            textShadowRadius={4}
+            numberOfLines={2}
+            style={styles.text}>
+            {children}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  componentWillUnmount () {
+    Animated.timing(this.animation, {
+      toValue: ANIMATION_INITIAL_VALUE,
+      duration: ANIMATION_DURATION_MS
+    })
+  }
+}
 
 ButtonWithImageBackground.propTypes = {
   backgroundSource: PropTypes.any.isRequired,
@@ -49,5 +81,3 @@ ButtonWithImageBackground.propTypes = {
     PropTypes.string.isRequired,
     PropTypes.arrayOf(PropTypes.string.isRequired).isRequired])
 }
-
-export default ButtonWithImageBackground
