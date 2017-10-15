4b5d69956c3addb9a112a6f183afff7bcb385852
Zofia Wisniowolska <zofia.wisniowolska.ctr@sabre.com>
2017-10-15 17:05:56 +0200

    Add more random backgrounds

--- src/utils/getRandomImageSource/index.js
+++ src/utils/getRandomImageSource/index.js
@@ -2,10 +2,18 @@ import randomItemInArray from 'random-item-in-array'
 
 const ALL_BACKGROUNDS = [
   require('./img/china.jpeg'),
-  require('./img/mountain.jpeg'),
-  require('./img/surfing.jpeg'),
+  require('./img/japan.jpeg'),
+  require('./img/rain.jpeg'),
+  require('./img/winter.jpeg'),
+  require('./img/elephant.jpeg'),
+  require('./img/lake.jpeg'),
+  require('./img/seagull.jpeg'),
+  require('./img/hipster.jpeg'),
   require('./img/mantis.jpeg'),
-  require('./img/seagull.jpeg')
+  require('./img/surfing.jpeg'),
+  require('./img/horses.jpeg'),
+  require('./img/mountain.jpeg'),
+  require('./img/venice.jpeg')
 ]
 
 function getRandomImageSource () {
